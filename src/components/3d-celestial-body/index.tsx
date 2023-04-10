37d29824c5abd132c01bdab5fb4380611004f679
LQYld <54627537+LQYld@users.noreply.github.com>
2023-04-11 00:40:15 +0800

    feat: add threejs package

--- src/components/3d-celestial-body/index.tsx
+++ src/components/3d-celestial-body/index.tsx
@@ -21,16 +21,7 @@ export default function ThreeDCelestialBody() {
 
     camera.position.z = 5
 
-    // function animate() {
-    //   requestAnimationFrame(animate)
-
-    //   cube.rotation.x += 0.01
-    //   cube.rotation.y += 0.01
-
     renderer.render(scene, camera)
-    // }
-
-    // animate()
   }, [])
   return <div id="container"></div>
 }
