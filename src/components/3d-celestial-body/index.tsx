0adc824cd1883b187030fd5501487e6bf125fba3
LQYld <54627537+LQYld@users.noreply.github.com>
2023-04-18 23:25:58 +0800

    feat: add 3d modal public

--- src/components/3d-celestial-body/index.tsx
+++ src/components/3d-celestial-body/index.tsx
@@ -1,27 +1,78 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
+import hdrFile from '../../../public/royal_esplanade_1k.hdr'
+import binFile from '../../../public/scene.bin'
+import gltfFile from '../../../public/scene.gltf'
+import styles from './index.module.css'
 export default function ThreeDCelestialBody() {
+  const webglDom = useRef(null)
   useEffect(() => {
-    const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    )
+    if (webglDom.current) {
+      const webglWidth = webglDom.current.offsetWidth
+      const webglHeight = webglDom.current.offsetHeight
 
-    const renderer = new THREE.WebGLRenderer()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(renderer.domElement)
+      // 创建场景
+      const scene = new THREE.Scene()
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-    const cube = new THREE.Mesh(geometry, material)
-    scene.add(cube)
+      // 设置环境光（十六进制颜色）
+      const ambient = new THREE.AmbientLight(0x444444)
 
-    camera.position.z = 5
+      // 将环境光添加到场景中
+      scene.add(ambient)
 
-    renderer.render(scene, camera)
+      // 设置点光源（十六进制颜色）
+      const point = new THREE.PointLight(0xffffff)
+
+      // 设置点光源的位置（x轴, y轴, z轴）
+      point.position.set(400, 200, 300)
+
+      // 将点光源添加到场景中
+      scene.add(point)
+
+      // 创建透视相机（角度, 宽高比, 最近距离, 最远距离）
+      const camera = new THREE.PerspectiveCamera(
+        60,
+        webglWidth / webglHeight,
+        0.1,
+        2000
+      )
+
+      // 设置相机的位置（x轴, y轴, z轴）
+      camera.position.set(100, 100, 100)
+
+      // 将相机指向场景中心
+      camera.lookAt(scene.position)
+
+      // 创建渲染器
+      const renderer = new THREE.WebGLRenderer()
+
+      // 设置渲染器的初始颜色（十六进制颜色, 透明度）
+      renderer.setClearColor(0xeeeeee, 1)
+
+      // 设置渲染器大小（标签宽度, 标签高度）
+      renderer.setSize(webglWidth, webglHeight)
+
+      // 将渲染器添加到渲染容器中（渲染器元素）
+      webglDom.current.appendChild(renderer.domElement)
+      renderer.render(scene, camera)
+
+      new RGBELoader().load(hdrFile, function (texture) {
+        texture.mapping = THREE.EquirectangularReflectionMapping
+
+        // scene.background = texture;
+        scene.environment = texture
+
+        const loader = new GLTFLoader()
+        loader.load(gltfFile, function (gltf) {
+          scene.add(gltf.scene)
+
+          renderer.render(scene, camera)
+        })
+      })
+    }
   }, [])
-  return <div id="container"></div>
+  return <div ref={webglDom} className={styles.container}></div>
 }
